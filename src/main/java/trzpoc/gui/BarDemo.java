a18ebeadc687d903586e43c8128cec821705c8ef
Luigi Talamona <luigi@talamona.org>
2017-05-30 09:33:38 +0200

    example for graphic bar

---
+++ src/main/java/trzpoc/gui/BarDemo.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2016 by Gerrit Grunwald
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package trzpoc.gui;
+
+import eu.hansolo.medusa.Gauge;
+import eu.hansolo.medusa.Gauge.SkinType;
+import eu.hansolo.medusa.GaugeBuilder;
+import eu.hansolo.medusa.Section;
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.geometry.Orientation;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.util.Random;
+
+
+public class BarDemo extends Application {
+    private static final Random RND            = new Random();
+    private static final double MIN_CELL_SIZE  = 80;
+    private static final double PREF_CELL_SIZE = 120;
+    private static final double MAX_CELL_SIZE  = 150;
+    private static       int    noOfNodes      = 0;
+    private Gauge          gauge21;
+    private AnimationTimer timer;
+    private long lastTimerCall;
+
+    @Override public void init() {
+        // TODO: isolate this
+        gauge21 = GaugeBuilder.create()
+                              .skinType(SkinType.LINEAR)
+                              //.title("Linear")
+                              .tickLabelsVisible(false)
+                              .tickLabelSectionsVisible(false)
+                              .tickMarkSectionsVisible(false)
+                              .orientation(Orientation.HORIZONTAL)
+                              .sectionsVisible(true)
+                              .valueVisible(false)
+                              .foregroundBaseColor(Color.BLUE)
+                              .barColor(Color.GREEN)
+                              .sections(new Section(0, 20, Color.BLUE),
+                                        new Section(80, 100, Color.RED),
+                                        new Section(50, 80, Color.ORANGE)
+                                      )
+                              .build();
+
+        lastTimerCall = System.nanoTime();
+        timer = new AnimationTimer() {
+            @Override public void handle(long now) {
+                if (now > lastTimerCall + 3_000_000_000l) {
+                    double value = RND.nextDouble() * gauge21.getRange() + gauge21.getMinValue();
+                    gauge21.setValue(value);
+                    if (value >= 60d){
+                        gauge21.setBarColor(Color.BLUE);
+                    }else{
+                        gauge21.setBarColor(Color.GREEN);
+                    }
+                    lastTimerCall = now;
+                }
+            }
+        };
+
+
+
+
+    }
+
+    @Override public void start(Stage stage) {
+        stage.setWidth(800d);
+        stage.setHeight(480d);
+        Group root = new Group();
+        gauge21.setPrefSize(700d, 100d);
+        gauge21.setLayoutX(10);
+        gauge21.setLayoutY(300);
+        root.getChildren().add(gauge21);
+        
+        //GridPane pane = new GridPane();
+        //pane.add(gauge21, 6, 1);
+        //pane.setHgap(10);
+        //pane.setVgap(10);
+        //pane.setBackground(new Background(new BackgroundFill(Color.rgb(90, 90, 90), CornerRadii.EMPTY, Insets.EMPTY)));
+        stage.setScene(new Scene(root));
+        stage.setTitle("TRZ bar for instruments");
+        stage.show();
+        timer.start();
+
+
+    }
+
+    @Override public void stop() {
+        System.exit(0);
+    }
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
