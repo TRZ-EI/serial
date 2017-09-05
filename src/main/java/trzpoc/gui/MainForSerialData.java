8381a74a8221f8c4222977e2f1cbcdc3a8f6f677
Luigi Talamona <luigi@talamona.org>
2017-09-05 13:48:59 +0200

    Totally refactored to emprove Canvas management

--- src/main/java/trzpoc/gui/MainForSerialData.java
+++ src/main/java/trzpoc/gui/MainForSerialData.java
@@ -8,23 +8,19 @@ package trzpoc.gui;
  */
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.beans.value.ObservableValue;
-import javafx.concurrent.Task;
-import javafx.scene.CacheHint;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import trzpoc.comunication.SerialDataManager;
-import trzpoc.gui.GraphicDesigner;
-import trzpoc.structure.CellsRow;
 import trzpoc.structure.serial.SerialDataFacade;
 import trzpoc.utils.ConfigurationHolder;
 import trzpoc.utils.FontAndColorSelector;
-
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * https://stackoverflow.com/a/44056730/230513
@@ -36,6 +32,7 @@ public class MainForSerialData extends Application {
     private FontAndColorSelector fontAndColorSelector;
     private SerialDataFacade serialDataFacade;
     private GraphicDesigner graphicDesigner;
+    private CanvasTask task;
 
     private static final int W = 800;
     private static final int H = 480;
@@ -58,24 +55,27 @@ public class MainForSerialData extends Application {
         StackPane root = new StackPane();
         Canvas canvas = new Canvas(W, H);
         Canvas canvasForGrid = new Canvas(W, H);
+        this.addMouseEventToExit(canvasForGrid);
+        canvasForGrid.toFront();
         root.getChildren().add(canvasForGrid);
 
         this.graphicDesigner.setDataDisplayManager(this.serialDataFacade.getDisplayManager());
         this.graphicDesigner.drawGrid(canvasForGrid.getGraphicsContext2D());
-
         root.getChildren().add(canvas);
         Scene scene = new Scene(root);
         stage.setScene(scene);
+        this.addCombinationKeyAcceleratorToExit(stage);
         stage.show();
-        CanvasTask task = new CanvasTask(this.graphicDesigner, this.serialDataFacade);
-        task.valueProperty().addListener((ObservableValue<? extends Canvas> observable, Canvas oldValue, Canvas newValue) -> {
+        this.task = new CanvasTask(this.graphicDesigner, this.serialDataFacade);
+        this.task.valueProperty().addListener((ObservableValue<? extends Canvas> observable, Canvas oldValue, Canvas newValue) -> {
             long start = System.nanoTime();
             root.getChildren().remove(oldValue);
             root.getChildren().add(newValue);
+            canvasForGrid.toFront();
             double elapsed = (System.nanoTime() - start) / 1_000d;
             System.out.println("Elapsed time: " + elapsed);
         });
-        Thread thread = new Thread(task);
+        Thread thread = new Thread(this.task);
         thread.setDaemon(true);
         thread.start();
     }
@@ -83,47 +83,31 @@ public class MainForSerialData extends Application {
         this.resourceFile = (!this.getParameters().getRaw().isEmpty())? this.getParameters().getRaw().get(0): DEFAULT_RESOURCE_FILE_NAME;
         this.configurationHolder = ConfigurationHolder.createSingleInstanceByConfigUri(this.resourceFile);
     }
-
-    private static class CanvasTask extends Task<Canvas> {
-
-        private final SerialDataFacade serialDataFacade;
-        private final GraphicDesigner graphicDesigner;
-        private boolean runningflag = true;
-        private BlockingQueue<String> serialBuffer;
-
-        public CanvasTask(GraphicDesigner graphicDesigner, SerialDataFacade serialDataFacade){
-            this.serialDataFacade = serialDataFacade;
-            this.serialBuffer = new LinkedBlockingQueue<String>(2048);
-            this.graphicDesigner = graphicDesigner;
-        }
-
-        @Override
-        protected Canvas call() throws Exception {
-            SerialDataManager sdm = SerialDataManager.createNewInstanceByQueue(this.serialBuffer);
-            sdm.connectToSerialPort();
-
-            Canvas canvas = null;
-            while (this.runningflag) {
-                while(!this.serialBuffer.isEmpty()){
-                    String message = this.serialBuffer.take();
-                    canvas = new Canvas(W, H);
-                    canvas.setCache(false);
-                    canvas.setCacheHint(CacheHint.QUALITY);
-
-                    GraphicsContext gc = canvas.getGraphicsContext2D();
-                    this.serialDataFacade.onSerialDataInput(message.getBytes());
-                    this.graphicDesigner.drawOnCanvas(serialDataFacade.getDisplayManager(), gc);
-                    updateValue(canvas);
+    private void addMouseEventToExit(final Canvas canvas){
+        canvas.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent me) {
+                if (me.getButton() == MouseButton.PRIMARY){
+                    try {
+                        task.setRunningflag(false);
+                        Platform.exit();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
+                me.consume();
             }
-            sdm.disconnectFromSerialPort();
-            return canvas;
-        }
-        public void setRunningflag(boolean runningflag) {
-            this.runningflag = runningflag;
-        }
+        });
     }
-
+    private void addCombinationKeyAcceleratorToExit(Stage primaryStage) {
+        primaryStage.getScene().getAccelerators().put(
+                KeyCombination.keyCombination("CTRL+C"),
+                new Runnable() {
+                    public void run() {
+                        Platform.exit();
+                    }
+                }
+        );
+    }
     public static void main(String[] args) {
         launch(args);
     }
