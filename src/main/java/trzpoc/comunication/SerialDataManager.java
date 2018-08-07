8d900eb96fa162f952dc36c89caa0a850fc358e0
luigi <luigi@talamona.org>
2018-08-07 17:49:55 +0200

    Removed inner class for serial data listener

--- src/main/java/trzpoc/comunication/SerialDataManager.java
+++ src/main/java/trzpoc/comunication/SerialDataManager.java
@@ -1,10 +1,6 @@
 package trzpoc.comunication;
 
 import com.fazecast.jSerialComm.SerialPort;
-import com.fazecast.jSerialComm.SerialPortDataListener;
-import com.fazecast.jSerialComm.SerialPortEvent;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
 import trzpoc.crc.CRC16CCITT;
 import trzpoc.crc.CRCCalculator;
 import trzpoc.crc.Crc16CcittKermit;
@@ -12,10 +8,7 @@ import trzpoc.gui.DrawingText;
 import trzpoc.structure.serial.MultipleCommandSplitter;
 import trzpoc.utils.ConfigurationHolder;
 
-import java.io.IOException;
-import java.util.List;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.LinkedBlockingQueue;
 
 /**
@@ -30,12 +23,7 @@ public class SerialDataManager {
     private SerialPort serialPort;
     private MultipleCommandSplitter multipleCommandSplitter;
     private BlockingQueue<String> serialBuffer;
-
     private DrawingText main;
-
-    // TODO: ONLY TO DEBUG - DELETE WHEN DONE
-    private long receivedCommands;
-
 
     public static SerialDataManager createNewInstance(){
         return new SerialDataManager();
@@ -49,8 +37,6 @@ public class SerialDataManager {
         this.serialBuffer = new LinkedBlockingQueue<>();
         this.multipleCommandSplitter = MultipleCommandSplitter.getNewInstance();
         this.NEW_LINE = this.readEndLineFromConfiguration();
-        // TODO: ONLY TO DEBUG - DELETE WHEN DONE
-        this.receivedCommands = 0;
     }
     private SerialDataManager(BlockingQueue<String> serialBuffer) {
         this.serialBuffer = serialBuffer;
@@ -63,86 +49,31 @@ public class SerialDataManager {
     }
 
 
-    public SerialPort connectToSerialPort() throws IOException{
-        // TODO: EXPORT PARAMS TO CONFIG FILE
-
-        this.serialPort = SerialPort.getCommPort("/dev/ttyUSB0");
-        this.serialPort.setBaudRate(115200);
-        this.serialPort.setComPortParameters(115200, 8, SerialPort.ONE_STOP_BIT, SerialPort.NO_PARITY);
-        this.serialPort.openPort();
-        if (serialPort != null){
-            System.out.println("Connected !!!");
-        }
-
+    public SerialPort connectToSerialPort(){
         try {
+            String serialPortName = ConfigurationHolder.getInstance().getProperties().getProperty(ConfigurationHolder.PORT);
+            String baudRateValue = ConfigurationHolder.getInstance().getProperties().getProperty(ConfigurationHolder.BAUD_RATE);
+            int baudRate = Integer.parseInt(baudRateValue, 10);
 
-            // TODO: EXTRACT LISTENER CLASS IN A DEDICATED FILE
-            serialPort.addDataListener(new SerialPortDataListener() {
-                int data = 0;
-                @Override
-                public int getListeningEvents() { return SerialPort.LISTENING_EVENT_DATA_AVAILABLE; }
-                @Override
-                public void serialEvent(SerialPortEvent event)
-                {
-                    if (event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE){
-                        return;
-                    }
-                    try {
-                        StringBuilder message = new StringBuilder();
-                        while ( ( data = serialPort.getInputStream().read()) > -1 ){
-                            if ( data == NEW_LINE ) {
-                                receivedCommands ++;
-                                break;
-                            }
-                            message.append((char) data);
-                        }
-                        if (message.toString().startsWith("^")) {
-
-                            // ONLY ONE MESSAGE
-                            boolean isValid = calculateCRC(new String[]{message.toString()});
-                            if (isValid) {
-
-                                //verifyStopAndCloseAndOpenSerialPort(message, receivedCommands);
-
-
-
-                                // TO MANAGE MULTIPLE COMMANDS IN A SINGLE ROW
-                                List<String> commands = multipleCommandSplitter.splitMultipleCommand(message.toString());
-                                System.out.println("Total commands received: " + receivedCommands);
-
-                                serialBuffer.addAll(commands);
-                                main.runAndWaitMyRunnable();
-
-                                //serialPort.getOutputStream().write(new String("OK").getBytes());
-                                //serialPort.getOutputStream().flush();
-                            }
-                            else{
-                                //serialPort.getOutputStream().write(new String("KO: " + message.toString() + NEW_LINE).getBytes());
-                                //serialPort.getOutputStream().flush();
-                            }
-                        }
-
-
-
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    } catch (ExecutionException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-
-
+            this.configureAndOpenSerialPort(serialPortName, baudRate);
+            if (serialPort != null){
+                System.out.println("Connected !!!");
+                this.serialPort.addDataListener(new TRZSerialPortListener(this, this.main));
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
         return this.serialPort;
     }
+    private void configureAndOpenSerialPort(String serialPortName, int baudRate) {
+        this.serialPort = SerialPort.getCommPort(serialPortName);
+        this.serialPort.setBaudRate(baudRate);
+        this.serialPort.setComPortParameters(baudRate, 8, SerialPort.ONE_STOP_BIT, SerialPort.NO_PARITY);
+        this.serialPort.openPort();
+    }
 
 
-    private boolean calculateCRC(String[] messages) {
+    protected boolean calculateCRC(String[] messages) {
         int score = 0;
         int crcDigits = 4; // 4 hex digits
         for (String message: messages){
@@ -191,5 +122,21 @@ public class SerialDataManager {
 
     public void setMain(DrawingText main){
         this.main = main;
+    }
+
+    public SerialPort getSerialPort() {
+        return this.serialPort;
+    }
+
+    public char getNEW_LINE() {
+        return this.NEW_LINE;
+    }
+
+    public BlockingQueue<String> getSerialBuffer() {
+        return this.serialBuffer;
+    }
+
+    public MultipleCommandSplitter getMultipleCommandSplitter() {
+        return this.multipleCommandSplitter;
     }
 }
