3d4c86a7d57058a98e306fbfe12851d5de5fa741
luigi <luigi@talamona.org>
2018-09-05 09:32:52 +0200

    Modified to configure and add TRZBar to graphical tree

--- src/main/java/trzpoc/structure/JfxGaugeBarUpdaterFragment.java
+++ src/main/java/trzpoc/structure/JfxGaugeBarUpdaterFragment.java
@@ -2,13 +2,14 @@ package trzpoc.structure;
 
 import eu.hansolo.medusa.Gauge;
 import javafx.scene.paint.Color;
+import trzpoc.gui.TRZBar;
 
 public class JfxGaugeBarUpdaterFragment implements RunnableFragment {
     private final Color color;
     private final double value;
-    private final Gauge gauge;
+    private final TRZBar gauge;
 
-    public JfxGaugeBarUpdaterFragment(Color color, double finalValue, Gauge t) {
+    public JfxGaugeBarUpdaterFragment(Color color, double finalValue, TRZBar t) {
         this.color = color;
         this.value = finalValue;
         this.gauge = t;
@@ -16,7 +17,7 @@ public class JfxGaugeBarUpdaterFragment implements RunnableFragment {
 
     @Override
     public void executeFragment() {
-        this.gauge.barColorProperty().set(color);
+        this.gauge.setColor(color);
         this.gauge.setValue(value);
     }
 }
