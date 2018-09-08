78687f6386ecdde14c76852d5d00cabfdae1e63d
luigi <luigi@talamona.org>
2018-09-08 11:37:06 +0200

    POmReader: various emprovments

--- src/main/java/trzpoc/utils/PomReader.java
+++ src/main/java/trzpoc/utils/PomReader.java
@@ -6,14 +6,71 @@ import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class PomReader {
-    public static void main(String[] args) throws IOException, XmlPullParserException {
-        MavenXpp3Reader reader = new MavenXpp3Reader();
-        Model model = reader.read(new FileReader("pom.xml"));
-        System.out.println(model.getId());
-        System.out.println(model.getGroupId());
-        System.out.println(model.getArtifactId());
-        System.out.println(model.getVersion());
+    private Model model;
+
+
+
+    public static PomReader getNewInstance() {
+        return new PomReader();
+    }
+    public static PomReader getNewInstanceByPomFile(String pomFile) {
+        return new PomReader(pomFile);
+    }
+    public static PomReader getNewInstanceByInputStream(InputStream stream) {
+        return new PomReader(stream);
+    }
+
+
+
+    public PomReader(){
+        try {
+            MavenXpp3Reader reader = new MavenXpp3Reader();
+            this.model = reader.read(new FileReader("pom.xml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public PomReader(String pomFile) {
+        try {
+            MavenXpp3Reader reader = new MavenXpp3Reader();
+            this.model = reader.read(new FileReader(pomFile));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public PomReader(InputStream stream) {
+        try {
+            MavenXpp3Reader reader = new MavenXpp3Reader();
+            this.model = reader.read(stream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public String readProjectVersionFromPom() {
+        return model.getVersion();
+    }
+    public String readGroupIdFromPom() {
+        return model.getGroupId();
+    }
+    public String readArtifactIdFromPom() {
+        return model.getArtifactId();
+    }
+    public String getPropertyByKeyFromPomProperties(String key){
+        return model.getProperties().getProperty(key);
+
     }
 }
